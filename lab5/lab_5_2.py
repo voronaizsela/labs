051601097cb3d04ab30f57dd21480b771019d970
voronaizsela <voronaizsela@gmail.com>
2024-05-17 12:53:07 +0300

    filter

--- lab5/lab_5_2.py
+++ lab5/lab_5_2.py
@@ -20,12 +20,12 @@ def harmonic(t, amplitude, frequency, phase, show_noise, sigma, noise_value):
     else:
         return clean_harmonic
 
-def median_filter(data, window_size):
-    filtered_data = np.zeros_like(data)
-    half_window = window_size // 2
-    for i in range(half_window, len(data) - half_window):
-        filtered_data[i] = np.median(data[i - half_window: i + half_window + 1])
-    return filtered_data
+def mediana(data, fullsize):
+    medfil = np.zeros_like(data)
+    half = fullsize // 2
+    for i in range(half, len(data) - half):
+        medfil[i] = np.median(data[i - half: i + half + 1])
+    return medfil
 
 app = dash.Dash(__name__)
 
@@ -56,7 +56,7 @@ app.layout = html.Div([
             dcc.Slider(id='noise-covariance-slider', min=0, max=1, step=0.01, value=noisecov_st,
                        marks={i/100: str(i/100) for i in range(0, 101, 10)}, className='slider'),
             html.Label('Розмір вікна для медіанного фільтра:'),
-            dcc.Dropdown(id='median-window-dropdown', options=[{'label': str(size), 'value': size} for size in range(3, 13)], value=3)
+            dcc.Dropdown(id='median-window-dropdown', options=[{'label': str(size), 'value': size} for size in range(3, 25)], value=3)
         ], style={'width': '40%', 'float': 'left'})
     ])
 ])
@@ -95,7 +95,7 @@ def update_noise_data(noisemean, noisecov):
 )
 def update_plots(amplitude, frequency, phase, show_noise, window_size, noise_value):
     noisy_signal = harmonic(t, amplitude, frequency, phase, show_noise, sigma_st, noise_value)
-    filtered_signal = median_filter(noisy_signal, window_size)
+    filtered_signal = mediana(noisy_signal, window_size)
 
     plot1 = go.Scatter(x=t, y=noisy_signal, mode='lines', name='Гармоніка з шумом', line=dict(color='purple'))
     layout1 = go.Layout(title='Гармоніка з шумом', xaxis_title='Час', yaxis_title='Амплітуда')
